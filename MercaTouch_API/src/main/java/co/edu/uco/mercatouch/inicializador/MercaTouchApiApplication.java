423c851d205014042f96a02b27a160e5454a5b0e
estebancoloradogonzalez <estebancoloradogonzalez@gmail.com>
2021-10-05 23:56:00 -0500

    Inicio del Proyecto

---
+++ MercaTouch_API/src/main/java/co/edu/uco/mercatouch/inicializador/MercaTouchApiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.mercatouch.inicializador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaTouchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaTouchApiApplication.class, args);
+	}
+
+}
---
+++ MercaTouch_API/src/test/java/co/edu/uco/mercatouch/inicializador/MercaTouchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.mercatouch.inicializador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaTouchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
